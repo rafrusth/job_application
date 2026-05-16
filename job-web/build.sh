5cc19be669e3684874b109084edd9e35affe56bf
rafrusth <rafifsetya41@gmail.com>
2026-05-16 22:35:35 +0700

    rahhhh

--- job-web/build.sh
+++ job-web/build.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -e
 
-composer install --no-dev --optimize-autoloader
+echo "Downloading Composer..."
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+chmod +x /usr/local/bin/composer
+
+echo "Installing PHP dependencies..."
+composer install --no-dev --optimize-autoloader --no-interaction
+
+echo "Caching Laravel config..."
 php artisan config:cache
 php artisan route:cache
+
+echo "Running migrations..."
 php artisan migrate --force
